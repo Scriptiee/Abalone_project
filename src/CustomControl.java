9be7cedb6a8a203585fb6cd3f8aca17aabc8a14d
Tiarhai <garyedwardthomas@yahoo.ie>
2016-12-01 03:19:57 +0000

    removed mouseEvent

--- src/CustomControl.java
+++ src/CustomControl.java
@@ -5,26 +5,12 @@ import javafx.scene.control.Control;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 
 public class CustomControl extends Control {
 
 	//Constructor
 	public CustomControl(){
 		setSkin(new CustomControlSkin(this));
-
-		// add a mouse clicked listener that will detect is shift is pressed or not, and add or move pieces
-		setOnMouseClicked(new EventHandler<MouseEvent>() {
-			// overridden handle method
-			@Override
-			public void handle(MouseEvent event) {
-				if(event.isShiftDown()){ // if shift key is pressed
-					System.out.println("Shift-mouse clicked at: X"+event.getX()+" Y"+event.getY()); // Test code - should call movePieces method in AbaloneBoard() TODO
-				} else if(!event.isShiftDown()){ // if shift key is not pressed
-					System.out.println("Mouse clicked at: X"+event.getX()+" Y"+event.getY()); // Test code - should call addSelectedPieces method in AbaloneBoard() TODO
-				}
-			}
-		});
 		
 		// add a key listener for SPACE that will reset the game
 		setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -37,18 +23,9 @@ public class CustomControl extends Control {
 			}
 		});
 	}
-
-	// override the resize method
-	@Override
-	public void resize(double width, double height) {
-		// update the size of the rectangle
-		super.resize(width, height);
-	}
 }
 
 /* -----STILL NEEDS----- 
  * (take a comment and work on it)
- * finish shift-mouse clicked method
- * finish mouse clicked method
  * finish space key reset method
  */
