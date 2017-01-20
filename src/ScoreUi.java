f14d29345922e13ecc948763d7ded80b2517eb30
Tiarhai <garyedwardthomas@yahoo.ie>
2017-01-20 21:21:18 +0000

    p1 pic fix

--- src/ScoreUi.java
+++ src/ScoreUi.java
@@ -10,23 +10,21 @@ import javafx.scene.shape.Polyline;
 public class ScoreUi {
 	
 	private Circle picture = new Circle(60);
-	private static StackPane playerPane = new StackPane();
+	private StackPane playerPane = new StackPane();
 	private Pane player = new Pane();
 	private Pane playerColorLine = new Pane();
 	private Label playerName = new Label();
 	private Polyline playerLine = new Polyline(50.0, 10.0, 250.0, 10.0);
-	private Label timer = new Label();
 	
 	public ScoreUi(Color c, String name, String id) {
 		picture.setFill(Color.TRANSPARENT);
 		playerName.setText(name);
 		playerName.setId(name.replaceAll(" ", ""));
-		timer.setText("0");
 		playerLine.setStroke(c);
 		playerLine.setStrokeWidth(5);
 		playerColorLine.getChildren().add(playerLine);
 		player.setId(id);
-		playerPane.getChildren().addAll(picture, player, timer);
+		playerPane.getChildren().addAll(picture, player);
 		playerPane.setPadding(new Insets(15,15,15,15));
 	}
 	
@@ -40,13 +38,5 @@ public class ScoreUi {
 	
 	public Pane getLine() {
 		return playerColorLine;
-	}
-	
-	public void updateTime(String time){
-		for (int i = 0; i < playerPane.getChildren().size(); i++){
-			if (playerPane.getChildren().get(i) == timer) playerPane.getChildren().remove(i);
-		}
-		timer.setText(time);
-		playerPane.getChildren().add(timer);
 	}
 }
